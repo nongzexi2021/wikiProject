06a858b582493775284d1112e1e369814e366f33
nongzexi2021 <nzxnongzexi@outlook.com>
2022-01-15 17:03:47 -0800

    增加自定义配置项

--- src/main/java/com/zexi/wiki/controller/TestController.java
+++ src/main/java/com/zexi/wiki/controller/TestController.java
@@ -1,20 +1,21 @@
 package com.zexi.wiki.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 //@RestController return a string; @Controller: return a page
 @RestController
 
 public class TestController {
+    @Value("${test.hello:TEST}")
+    private String testHello;
     //GET POST PUT DELETE
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello World" + testHello;
     }
     @PostMapping("/hello/post")
     public String helloPost(String name){
