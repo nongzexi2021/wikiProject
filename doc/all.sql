4c4994f72242caec8a3cafa59eeb56998f6604b1
nongzexi2021 <nzxnongzexi@outlook.com>
2022-04-04 17:20:23 -0700

    集成mybatis generator, 生成demo表代码

--- doc/all.sql
+++ doc/all.sql
@@ -5,3 +5,15 @@ create table `test` (
                         `password` varchar (50) comment 'password',
                         primary key  (`id`)
 )engine=innodb default charset=utf8mb4 comment'test';
+
+insert into `test` (id, name, password) values(1,'测试','password' );
+
+drop table if exists `demo`;
+create table `demo` (
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    primary key (`id`)
+
+) engine=innodb default charset=utf8mb4 comment'测试';
+
+insert into `demo` (id, name) values(1,'测试');
