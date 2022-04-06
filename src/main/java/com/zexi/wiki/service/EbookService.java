dbc2a1124db79be4d553736f76b89520a2b0fd1d
nongzexi2021 <nzxnongzexi@outlook.com>
2022-04-06 11:45:26 -0700

    列表查询接口支持动态SQL

--- src/main/java/com/zexi/wiki/service/EbookService.java
+++ src/main/java/com/zexi/wiki/service/EbookService.java
@@ -7,6 +7,7 @@ import com.zexi.wiki.req.EbookReq;
 import com.zexi.wiki.resp.EbookResp;
 import com.zexi.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -20,7 +21,10 @@ public class EbookService {
     public List<EbookResp> list(EbookReq req){
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%" + req.getName() + "%");
+        if(!ObjectUtils.isEmpty(req.getName())){
+            criteria.andNameLike("%" + req.getName() + "%");
+        }
+
         List<Ebook> ebookList =  ebookMapper.selectByExample(ebookExample);
 
         List<EbookResp> respList = new ArrayList<>();
