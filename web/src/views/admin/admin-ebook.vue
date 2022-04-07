286c222bcbd5b18fe1794ac0bad8f5a76ede6b31
nongzexi2021 <nzxnongzexi@outlook.com>
2022-04-07 12:15:48 -0700

    点击编辑按钮时弹出编辑模块框

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -16,7 +16,7 @@
         </template>
         <template v-slot:action="{ text, record }">
           <a-space size="small">
-            <a-button type="primary">
+            <a-button type="primary" @click="edit">
               编辑
             </a-button>
             <a-button type="danger">
@@ -27,6 +27,15 @@
       </a-table>
     </a-layout-content>
   </a-layout>
+
+  <a-modal
+      title="电子书表单"
+      v-model:visible="modalVisible"
+      :confirm-loading="modalLoading"
+      @ok="handleModalOk"
+  >
+    <p>test</p>
+  </a-modal>
 </template>
 
 <script lang="ts">
@@ -87,11 +96,12 @@ export default defineComponent({
      **/
     const handleQuery = (params: any) => {
       loading.value = true;
-      axios.get("/ebook/list", {params: {
-        page: params.page,
-        size: params.size
+      axios.get("/ebook/list", {
+        params: {
+          page: params.page,
+          size: params.size
         }
-    }).then((response) => {
+      }).then((response) => {
         loading.value = false;
         const data = response.data;
         ebooks.value = data.content.list;
@@ -113,11 +123,28 @@ export default defineComponent({
       });
     };
 
+    // -------- 表单 ---------
+    const modalVisible = ref(false);
+    const modalLoading = ref(false);
+    const handleModalOk = () => {
+      modalLoading.value = true;
+      setTimeout(() => {
+        modalVisible.value = false;
+        modalLoading.value = false;
+      }, 2000);
+    };
+
+    /**
+     * 编辑
+     */
+    const edit = () => {
+      modalVisible.value = true;
+    };
+
     onMounted(() => {
-      console.log("看看自带的分页参数有什么： " + pagination);
       handleQuery({
-        page:1,
-        size: pagination.value.pageSize;
+        page: 1,
+        size: pagination.value.pageSize,
       });
     });
 
@@ -126,7 +153,13 @@ export default defineComponent({
       pagination,
       columns,
       loading,
-      handleTableChange
+      handleTableChange,
+
+      edit,
+
+      modalVisible,
+      modalLoading,
+      handleModalOk
     }
   }
 });
