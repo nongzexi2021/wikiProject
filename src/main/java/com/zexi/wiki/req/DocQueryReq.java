d49ad82d89e8e7627be07edf2d76228e8ec63b05
nongzexi2021 <nzxnongzexi@outlook.com>
2022-04-10 09:17:02 -0700

    从分类管理复制出文档管理的前后端代码

---
+++ src/main/java/com/zexi/wiki/req/DocQueryReq.java
@@ -0,0 +1,8 @@
+package com.zexi.wiki.req;
+
+public class DocQueryReq extends PageReq {
+    @Override
+    public String toString() {
+        return "DocQueryReq{} " + super.toString();
+    }
+}
