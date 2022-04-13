181142423f24c500d36fc69b25a22a3f7759603e
nongzexi2021 <nzxnongzexi@outlook.com>
2022-04-12 22:44:22 -0700

    增加校验用户ing不能重复，增加自定义异常

---
+++ src/main/java/com/zexi/wiki/exception/BusinessExceptionCode.java
@@ -0,0 +1,22 @@
+package com.zexi.wiki.exception;
+
+public enum BusinessExceptionCode {
+
+    USER_LOGIN_NAME_EXIST("user login name already exited!"),
+    LOGIN_USER_ERROR("username does not exist or wrong password!")
+    ;
+
+    private String desc;
+
+    BusinessExceptionCode(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
