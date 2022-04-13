e1fd494361d68f302d92eaf490f1d373aaaaff4d
nongzexi2021 <nzxnongzexi@outlook.com>
2022-04-12 22:37:21 -0700

    完成用户表基本增删改查功能

---
+++ src/main/java/com/zexi/wiki/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.zexi.wiki.controller;
+
+import com.zexi.wiki.req.UserQueryReq;
+import com.zexi.wiki.req.UserSaveReq;
+import com.zexi.wiki.resp.CommonResp;
+import com.zexi.wiki.resp.UserQueryResp;
+import com.zexi.wiki.resp.PageResp;
+import com.zexi.wiki.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    @GetMapping("/list")
+    public CommonResp list(@Valid UserQueryReq req) {
+        CommonResp<PageResp<UserQueryResp>> resp = new CommonResp<>();
+        PageResp<UserQueryResp> list = userService.list(req);
+        resp.setContent(list);
+        return resp;
+    }
+
+    @PostMapping("/save")
+    public CommonResp save(@Valid @RequestBody UserSaveReq req) {
+        CommonResp resp = new CommonResp<>();
+        userService.save(req);
+        return resp;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id) {
+        CommonResp resp = new CommonResp<>();
+        userService.delete(id);
+        return resp;
+    }
+}
